f2441dbd6a2c1549e62db72d694de81bb30b15c9
suojae3 <suojae3@gmail.com>
2023-09-11 20:05:15 +0900

    팟파일 설치, 스토리보드 삭제, 네비게이션 뷰컨 설정

---
+++ SimpleWeather/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by ㅣ on 2023/09/11.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setpuUI()
+    }
+
+    private func setpuUI() {
+        view.backgroundColor = .white
+    }
+    
+    
+    
+    
+
+}
+
